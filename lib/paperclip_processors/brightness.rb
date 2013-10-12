52ff7603169453434683f177a272374fa24239a7
bizarre <b@tumblbug.com>
2013-10-12 18:17:51 +0900

    밝기조정

--- lib/paperclip_processors/brightness.rb
+++ lib/paperclip_processors/brightness.rb
@@ -16,7 +16,7 @@ module Paperclip
       begin
         parameters = []
         parameters << ":source"
-        parameters << "-brightness-contrast 2x2"
+        parameters << "-brightness-contrast 5x10"
         parameters << ":dest"
 
         parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
